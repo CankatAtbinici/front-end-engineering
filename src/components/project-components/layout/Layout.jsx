d7b7ce4c2cc26f48eb67d6ebc35dd5ea61a67508
Cankat Atbinici <cankatbinici@gmail.com>
2023-04-22 17:42:22 +0300

    useScrool custom hook created and entegred project

--- src/components/project-components/layout/Layout.jsx
+++ src/components/project-components/layout/Layout.jsx
@@ -1,12 +1,15 @@
-import React from 'react';
-import Header from '../header/index';
-
+import React from "react";
+import Header from "../header/index";
+import UseScrollToTop from "../../../hooks/useScrool";
+import FooterProject from "../footer/index";
 
 function Layout(props) {
   return (
     <>
       <Header />
       {props.children}
+      <UseScrollToTop aria-label="Scrool to top of page" />
+      <FooterProject />
     </>
   );
 }
