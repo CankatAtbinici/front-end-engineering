6056346def08d72240831a1d95cc10ace90a1cd9
Cankat Atbinici <cankatbinici@gmail.com>
2023-04-22 17:39:44 +0300

    skills page and components created and designed

--- src/pages/skills/Index.jsx
+++ src/pages/skills/Index.jsx
@@ -1,12 +1,20 @@
-import React from 'react';
+import React from "react";
+import { skillsData } from "../../fakeData/fakeData";
+import SingleSkill from "../../components/project-components/skills/singleSkill";
 
 function Skills(props) {
-    return (
-        <div>
-            Burası Skills sayfasıdır.
-        </div>
-    );
+  return (
+    <section className = {"container mx-auto"} >
+      {skillsData.map((data) => (
+        <SingleSkill
+          title={data.title} 
+          key={data.id} 
+          description={data.desc}
+          icon = {data.component}
+        />
+      ))}
+    </section>
+  );
 }
 
 export default Skills;
-
